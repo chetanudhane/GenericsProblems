f4f9d3d301242f2f055a1f8d4021fb789f888025
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-30 20:53:46 +0530

    Refactor2 Added

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -4,6 +4,17 @@ public class FindMaximumNumber<T extends Comparable<T>> {
 
 	T x, y, z;
 
+	public FindMaximumNumber(T x, T y, T z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T maximum() {
+		return FindMaximumNumber.maximumIs(x, y, z);
+	}
+
 	public static <T extends Comparable<T>> T maximumIs(T x, T y, T z) {
 		T max = x;
 		if (y.compareTo(max) > 0) {
--- MaxNumberMain.java
+++ MaxNumberMain.java
@@ -4,9 +4,17 @@ public class MaxNumberMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("Maximum Integer Number is " + FindMaximumNumber.maximumIs(45, 12, 34));
-		System.out.println("Maximum Float Number is " + FindMaximumNumber.maximumIs(43.33, 23.45, 56.23));
-		System.out.println("Maximum String is " + FindMaximumNumber.maximumIs("Mumbai", "Pune", "India"));
+		Integer x = 45, y = 12, z = 34;
+		int maxInt = new FindMaximumNumber<Integer>(x, y, z).maximum();
+		System.out.println("Maximum is : " + maxInt);
+
+		Double x1 = 81.2, y1 = 73.45, z1 = 65.4;
+		double maxDouble = new FindMaximumNumber<Double>(x1, y1, z1).maximum();
+		System.out.println("Maximum is : " + maxDouble);
+
+		String x11 = "Mumbai", y11 = "Pune", z11 = "India";
+		String maxString = new FindMaximumNumber<String>(x11, y11, z11).maximum();
+		System.out.println("Maximum is : " + maxString);
 
 	}
 
