082fa1154b3889abfe8264c548dbd6892c24f4d8
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-30 20:07:42 +0530

    Refactor1 Added

---
+++ FindMaximumNumber.java
@@ -0,0 +1,17 @@
+package generics;
+
+public class FindMaximumNumber<T extends Comparable<T>> {
+
+	T x, y, z;
+
+	public static <T extends Comparable<T>> T maximumIs(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+}
---
+++ MaxNumberMain.java
@@ -0,0 +1,13 @@
+package generics;
+
+public class MaxNumberMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Maximum Integer Number is " + FindMaximumNumber.maximumIs(45, 12, 34));
+		System.out.println("Maximum Float Number is " + FindMaximumNumber.maximumIs(43.33, 23.45, 56.23));
+		System.out.println("Maximum String is " + FindMaximumNumber.maximumIs("Mumbai", "Pune", "India"));
+
+	}
+
+}
