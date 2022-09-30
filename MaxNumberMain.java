11ace5e5ec0f547a1846a614acc1647917fed694
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-30 19:34:54 +0530

    Find Maximum Integer

---
+++ MaxNumberMain.java
@@ -0,0 +1,10 @@
+package generics;
+
+public class MaxNumberMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Maximum Number is " + FindMaximumNumber.findMaximumNumber(39, 50, 12));
+	}
+
+}
