5128ea681d0ca573e54eb68d14469f5cf1c6d72d
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-30 19:39:46 +0530

    Find Maximum Float Number

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -2,8 +2,8 @@ package generics;
 
 public class FindMaximumNumber {
 
-	public static Integer findMaximumNumber(Integer x, Integer y, Integer z) {
-		Integer max = x;
+	public static Float findMaximumNumber(Float x, Float y, Float z) {
+		Float max = x;
 		if (y.compareTo(max) > 0) {
 			max = y;
 		}
--- MaxNumberMain.java
+++ MaxNumberMain.java
@@ -4,7 +4,7 @@ public class MaxNumberMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("Maximum Number is " + FindMaximumNumber.findMaximumNumber(39, 50, 12));
+		System.out.println("Maximum Number is " + FindMaximumNumber.findMaximumNumber(70.9f,59.3f,80.5f));
 	}
 
 }
